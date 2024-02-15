5e6ae8e1f666513c9e6736e2ff4b175137969ae2
takeaflxwer99 <anghelteo0@gmail.com>
2024-02-15 09:36:35 +0100

    8Puzzle
    hola

---
+++ Assets/Scripts/ST_PuzzleDisplay.cs
@@ -0,0 +1,270 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ST_PuzzleDisplay : MonoBehaviour 
+{
+	public Texture PuzzleImage;
+
+	public int Height = 3;
+	public int Width  = 3;
+
+
+	public Vector3 PuzzleScale = new Vector3(1.0f, 1.0f, 1.0f);
+	public Vector3 PuzzlePosition = new Vector3(0.0f, 0.0f, 0.0f);
+	public float SeperationBetweenTiles = 0f;
+	public GameObject Tile;
+
+	public Shader PuzzleShader;
+
+	private GameObject[,] TileDisplayArray;
+	private List<Vector3>  DisplayPositions = new List<Vector3>();
+
+	private Vector3 Scale;
+	private Vector3 Position;
+
+
+	public bool Complete = false;
+
+
+	void Start () 
+	{
+
+		CreatePuzzleTiles();
+
+		StartCoroutine(JugglePuzzle());
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.localPosition = PuzzlePosition;
+
+		this.transform.localScale = PuzzleScale;
+	}
+
+	public Vector3 GetTargetLocation(ST_PuzzleTile thisTile)
+	{
+		ST_PuzzleTile MoveTo = CheckIfWeCanMove((int)thisTile.GridLocation.x, (int)thisTile.GridLocation.y, thisTile);
+
+		if(MoveTo != thisTile)
+		{
+			Vector3 TargetPos = MoveTo.TargetPosition;
+			Vector2 GridLocation = thisTile.GridLocation;
+			thisTile.GridLocation = MoveTo.GridLocation;
+
+			MoveTo.LaunchPositionCoroutine(thisTile.TargetPosition);
+			MoveTo.GridLocation = GridLocation;
+
+			return TargetPos;
+		}
+
+		return thisTile.TargetPosition;
+	}
+
+	private ST_PuzzleTile CheckMoveLeft(int Xpos, int Ypos, ST_PuzzleTile thisTile)
+	{
+		if((Xpos - 1)  >= 0)
+		{
+			// esta la posición usada?
+			return GetTileAtThisGridLocation(Xpos - 1, Ypos, thisTile);
+		}
+		
+		return thisTile;
+	}
+	
+	private ST_PuzzleTile CheckMoveRight(int Xpos, int Ypos, ST_PuzzleTile thisTile)
+	{
+		if((Xpos + 1)  < Width)
+		{
+			return GetTileAtThisGridLocation(Xpos + 1, Ypos , thisTile);
+		}
+		
+		return thisTile;
+	}
+	
+	private ST_PuzzleTile CheckMoveDown(int Xpos, int Ypos, ST_PuzzleTile thisTile)
+	{
+		if((Ypos - 1)  >= 0)
+		{
+			return GetTileAtThisGridLocation(Xpos, Ypos  - 1, thisTile);
+		}
+		
+		return thisTile;
+	}
+	
+	private ST_PuzzleTile CheckMoveUp(int Xpos, int Ypos, ST_PuzzleTile thisTile)
+	{
+		if((Ypos + 1)  < Height)
+		{
+			return GetTileAtThisGridLocation(Xpos, Ypos  + 1, thisTile);
+		}
+		
+		return thisTile;
+	}
+	
+	private ST_PuzzleTile CheckIfWeCanMove(int Xpos, int Ypos, ST_PuzzleTile thisTile)
+	{
+		if(CheckMoveLeft(Xpos, Ypos, thisTile) != thisTile)
+		{
+			return CheckMoveLeft(Xpos, Ypos, thisTile);
+		}
+		
+		if(CheckMoveRight(Xpos, Ypos, thisTile) != thisTile)
+		{
+			return CheckMoveRight(Xpos, Ypos, thisTile);
+		}
+		
+		if(CheckMoveDown(Xpos, Ypos, thisTile) != thisTile)
+		{
+			return CheckMoveDown(Xpos, Ypos, thisTile);
+		}
+		
+		if(CheckMoveUp(Xpos, Ypos, thisTile) != thisTile)
+		{
+			return CheckMoveUp(Xpos, Ypos, thisTile);
+		}
+
+		return thisTile;
+	}
+
+	private ST_PuzzleTile GetTileAtThisGridLocation(int x, int y, ST_PuzzleTile thisTile)
+	{
+		for(int j = Height - 1; j >= 0; j--)
+		{
+			for(int i = 0; i < Width; i++)
+			{
+				if((TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>().GridLocation.x == x)&&
+				   (TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>().GridLocation.y == y))
+				{
+					if(TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>().Active == false)
+					{
+						return TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>();
+					}
+				}
+			}
+		}
+
+		return thisTile;
+	}
+
+	private IEnumerator JugglePuzzle()
+	{
+		yield return new WaitForSeconds(1.0f);
+
+		// ocultar una pieza
+		TileDisplayArray[0,0].GetComponent<ST_PuzzleTile>().Active = false;
+
+		yield return new WaitForSeconds(1.0f);
+
+		for(int k = 0; k < 20; k++)
+		{
+			// posicionar random cada pieza
+			for(int j = 0; j < Height; j++)
+			{
+				for(int i = 0; i < Width; i++)
+				{		
+					TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>().ExecuteAdditionalMove();
+
+					yield return new WaitForSeconds(0.02f);
+				}
+			}
+		}
+
+		StartCoroutine(CheckForComplete());
+
+		yield return null;
+	}
+
+	public IEnumerator CheckForComplete()
+	{
+		while(Complete == false)
+		{
+			Complete = true;
+			for(int j = Height - 1; j >= 0; j--)
+			{
+				for(int i = 0; i < Width; i++)
+				{
+					if(TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>().CorrectLocation == false)  
+					{
+						Complete = false;
+					}
+				}
+			}
+
+			yield return null;
+		}
+				
+		// Depues de ganar
+		if(Complete)
+		{
+			Debug.Log("Puzzle Complete!");
+			//Volver a escena principal
+		}
+
+		yield return null;
+	}
+
+	private Vector2 ConvertIndexToGrid(int index)
+	{
+		int WidthIndex = index;
+		int HeightIndex = 0;
+
+		for(int i = 0; i < Height; i++)
+		{
+			if(WidthIndex < Width)
+			{
+				return new Vector2(WidthIndex, HeightIndex);
+			}
+			else
+			{
+				WidthIndex -= Width;
+				HeightIndex++;
+			}
+		}
+
+		return new Vector2(WidthIndex, HeightIndex);
+	}
+
+	private void CreatePuzzleTiles()
+	{
+		TileDisplayArray = new GameObject[Width,Height];
+
+		Scale = new Vector3(1.0f/Width, 1.0f, 1.0f/Height);
+		Tile.transform.localScale = Scale;
+
+		int TileValue = 0;
+
+		for(int j = Height - 1; j >= 0; j--)
+		{
+			for(int i = 0; i < Width; i++)
+			{
+				Position = new Vector3(((Scale.x * (i + 0.5f))-(Scale.x * (Width/2.0f))) * (10.0f + SeperationBetweenTiles), 
+				                       0.0f, 
+				                      ((Scale.z * (j + 0.5f))-(Scale.z * (Height/2.0f))) * (10.0f + SeperationBetweenTiles));
+
+				DisplayPositions.Add(Position);
+
+				TileDisplayArray[i,j] = Instantiate(Tile, new Vector3(0.0f, 0.0f, 0.0f) , Quaternion.Euler(90.0f, -180.0f, 0.0f)) as GameObject;
+				TileDisplayArray[i,j].gameObject.transform.parent = this.transform;
+
+				ST_PuzzleTile thisTile = TileDisplayArray[i,j].GetComponent<ST_PuzzleTile>();
+				thisTile.ArrayLocation = new Vector2(i,j);
+				thisTile.GridLocation = new Vector2(i,j);
+				thisTile.LaunchPositionCoroutine(Position);
+				TileValue++;
+
+				Material thisTileMaterial = new Material(PuzzleShader);
+
+				thisTileMaterial.mainTexture = PuzzleImage;
+					
+				thisTileMaterial.mainTextureOffset = new Vector2(1.0f/Width * i, 1.0f/Height * j);
+				thisTileMaterial.mainTextureScale  = new Vector2(1.0f/Width, 1.0f/Height);
+					
+				TileDisplayArray[i,j].GetComponent<Renderer>().material = thisTileMaterial;
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/ST_PuzzleTile.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ST_PuzzleTile : MonoBehaviour 
+{
+	public Vector3 TargetPosition;
+
+	public bool Active = true;
+
+	public bool CorrectLocation = false;
+
+	public Vector2 ArrayLocation = new Vector2();
+	public Vector2 GridLocation = new Vector2();
+
+	void Awake()
+	{
+		TargetPosition = this.transform.localPosition;
+
+		StartCoroutine(UpdatePosition());
+	}
+
+	public  void LaunchPositionCoroutine(Vector3 newPosition)
+	{
+		TargetPosition = newPosition;
+
+		StartCoroutine(UpdatePosition());
+	}
+
+	public IEnumerator UpdatePosition()
+	{
+		while(TargetPosition != this.transform.localPosition)
+		{
+			this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, TargetPosition, 10.0f * Time.deltaTime);
+			yield return null;
+		}
+
+		if(ArrayLocation == GridLocation){CorrectLocation = true;}else{CorrectLocation = false;}
+
+		if(Active == false)
+		{
+			this.GetComponent<Renderer>().enabled = false;
+			this.GetComponent<Collider>().enabled = false;
+		}
+
+		yield return null;
+	}
+
+	public void ExecuteAdditionalMove()
+	{
+		LaunchPositionCoroutine(this.transform.parent.GetComponent<ST_PuzzleDisplay>().GetTargetLocation(this.GetComponent<ST_PuzzleTile>()));
+	}
+
+	void OnMouseDown()
+	{
+		LaunchPositionCoroutine(this.transform.parent.GetComponent<ST_PuzzleDisplay>().GetTargetLocation(this.GetComponent<ST_PuzzleTile>()));
+	}
+}
